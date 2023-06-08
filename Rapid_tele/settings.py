e5b507349dfdad417e4b6335b3f6303a2e3e2be7
emerie <danielchiemerie21@gmail.com>
2023-06-08 22:23:43 +0100

    statics

--- Rapid_tele/settings.py
+++ Rapid_tele/settings.py
@@ -118,6 +118,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
